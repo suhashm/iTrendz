7c5f18da3cf740fb4b100574e4a80aa04c5e9e79
Suhas <suhas@host-1-50.mimpvbg.clients.pavlovmedia.com>
2014-10-30 01:39:31 -0500

    Changing callback for modulus

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,8 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '198405354360-t0vnnm6rr5nt1019tgkgknnr27aks4tv.apps.googleusercontent.com',
         'clientSecret'  : '_fgUf5NlA8p5Vqp88A-935G_',
-        'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+        //'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+        'callbackURL'   : 'http://itrendz.mod.bz/auth/google/callback'
     }
 
 };
